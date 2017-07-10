7176b287e7307855b8f92d5d67708f4c01c8b46e
Samer Azani <samer.azani@gmail.com>
2017-07-10 11:19:58 +0200

    refactor code and winning process

--- TicTacToeDemo/Scripts/default.js
+++ TicTacToeDemo/Scripts/default.js
@@ -2,7 +2,10 @@
 var MyTicTacToe = (function ($) {
     var GAME_HUB = $.connection.ticTacToeHub;
     var SQUARE_COUNT = 9;
-    var _playerLetter;
+    var INITIAL_TURNS = 9;
+    var MOBILE_SCREEN_SIZE = 550;
+
+    var _player;
     var _turnsLeft;
     var _resultX = 0;
     var _resultO = 0;
@@ -13,9 +16,45 @@ var MyTicTacToe = (function ($) {
     var _firstPlayer;
     var _secondPlayer;
 
-    var startGame = function () {
+    var getWinningTiles = function () {
+        return function (currentTile) {
+            var winning_tiles;
+            if (currentTile == 0 || currentTile == 8) {
+                winning_tiles = [["-4", "-8"], ["-3", "-6"], ["-1", "-2"], ["1", "2"], ["3", "6"], ["4", "8"]];
+            }
+            else if (currentTile == 1 || currentTile == 7) {
+                winning_tiles = [["-3", "-6"], ["-1", "1"], ["3", "6"]];
+            }
+            else if (currentTile == 2 || currentTile == 6) {
+                winning_tiles = [["-3", "-6"], ["-2", "-4"], ["-1", "-2"], ["1", "2"], ["2", "4"], ["3", "6"]];
+            }
+            else if (currentTile == 3 || currentTile == 5) {
+                winning_tiles = [["-3", "3"], ["-1", "-2"], ["1", "2"]];
+            }
+            else {
+                winning_tiles = [ ["-4", "4"], ["-3", "3"], ["-2", "2"], ["-1", "1"]];
+            }
+
+            return winning_tiles;
+        }         
+    };
+
+    var setGame = function () {
+        setPlayButton();
+        setBoard();        
+    }
+    return {
+        setGame: setGame,
+        startGame: startGame,
+        playTurn: playTurn
+    };
+
+    function setPlayButton() {
         $("#btnPlayAgain").css("display", "none");
         $('#btnPlayAgain').click(playAgain);
+    }
+
+    function setBoard() {
         if (isMobile()) {
             $('#gameBoard').css("cursor", "pointer");
             $('.tile').css("cursor", "default");
@@ -32,41 +71,94 @@ var MyTicTacToe = (function ($) {
         }
     }
 
-    return {
-        startGame: startGame,
-        permitMoves: permitMoves,
-        playTurn: playTurn
-    };
+    function isMobile() {
+        if (screen.width <= MOBILE_SCREEN_SIZE)
+            _isMobile = true;
+        else
+            _isMobile = false;
 
-    function permitMoves(currentPlayer, firstPlayer, secondPlayer) {
+        return _isMobile;
+    }
+
+    function createMobileTile(e) {
+        var firstPlayerTile = $('#tileX');
+        var secondPlayerTile = $('#tileO');
+        if (isOwnTurn(firstPlayerTile, _firstPlayer) || isOwnTurn(secondPlayerTile, _secondPlayer)) {
+            GAME_HUB.server.executeTurn("square" + $(e.target).data('square'), _player);
+        }
+    }
+
+    function isOwnTurn(playerTile, currentPlayer) {
+        if (playerTile.length && _currentPlayer == currentPlayer) {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function drop(e) {
+        e.preventDefault();
+        GAME_HUB.server.executeTurn(e.target.id, e.dataTransfer.getData('text'));
+    }
+
+    function dragStarted(e) {
+        var $tile = $(e.target);
+        $tile.addClass("dragged");
+        var sourceLocation = $tile.parent().data('square');
+        e.dataTransfer.setData('text', sourceLocation.toString());
+        e.dataTransfer.effectAllowed = 'move';
+    }
+
+    function dragEnded(e) {
+        $(e.target).removeClass("dragged");
+    }
+
+    function preventDefault(e) {
+        e.preventDefault();
+    }
+
+    function startGame(currentPlayer, firstPlayer, secondPlayer) {
         _currentPlayer = currentPlayer;
         _firstPlayer = firstPlayer;
         _secondPlayer = secondPlayer;
 
-        initializeRound();
+        setRound();
     }
 
-    function playTurn(target, tileType) {
-        var $square = $('#' + target);
-        if ($square.hasClass('square')) {
-            var destinationLocation = $square.data('square');
-            var sourceLocation = tileType;
-            moveTile(sourceLocation, destinationLocation);
-        }
-    }
-
-    function initializeRound() {
-        _turnsLeft = 9;
+    function setRound() {        
         resetBoard();
-        createBoard();
-        if (_playerLetter == "X") {
-            initializePlayer("O");
+
+        if (_player == "X") {
+            setPlayer("O");
         }
         else {
-            initializePlayer("X");
+            setPlayer("X");
         }
-        displayResults("");
+
+        setTurn();       
+    }
+
+    function resetBoard() {
+        _turnsLeft = INITIAL_TURNS;
+        displayRoundResults("");
         $("#btnPlayAgain").css("display", "none");
+        $('#gameBoard').children().remove();
+        $('.square').children().remove();
+        for (var i = 0; i < SQUARE_COUNT; i++) {
+            var square = $('<div id="square' + i + '" data-square="' + i + '" class="square" ></div>');
+            square.appendTo($('#gameBoard'));
+        }
+    }
+
+    function setPlayer(player) {
+        _player = player;
+        var square = $('#square' + player);
+        var tile = $('<div id="tile' + player + '"  draggable="true" class="tile" >' + player + '</div>');
+        tile.appendTo(square);
+        _turnsLeft--;
+    }
+
+    function setTurn() {
         if (_currentPlayer == _firstPlayer) {
             $('#squareO').on('dragstart', preventDefault);
         }
@@ -75,68 +167,89 @@ var MyTicTacToe = (function ($) {
         }
     }
 
-    function initializePlayer(player) {
-        _playerLetter = player;
-        var $square = $('#square' + player);
-        var $tile = $('<div id="tile' + player + '"  draggable="true" class="tile" >' + player + '</div>');
-        $tile.appendTo($square);
-        _turnsLeft--;
-    }
-
-    function createMobileTile(e) {
-        var $tileFirstPlayer = $('#tileX');
-        var $tileSecondPlayer = $('#tileO');
-        if (isPlayerTurn($tileFirstPlayer, _firstPlayer) || isPlayerTurn($tileSecondPlayer, _secondPlayer)) {
-            GAME_HUB.server.executeTurn("square" + $(e.target).data('square'), _playerLetter);
+    function playTurn(target, tileType) {
+        var square = $('#' + target);
+        if (square.hasClass('square')) {
+            var destination = square.data('square');
+            var source = tileType;
+            moveTile(source, destination);
         }
     }
 
-    function moveTile(sourceLocation, destinationLocation) {
-        var $draggedItem = $('#square' + sourceLocation).children();
-        $draggedItem.detach();
-        var $target = $('#square' + destinationLocation);
-        $draggedItem.appendTo($target);
-        var winningGame = checkForWinner(sourceLocation, destinationLocation);
-        if (winningGame) {
-            updateResults(sourceLocation);
+    function moveTile(source, destination) {
+        var draggedItem = $('#square' + source).children();
+        draggedItem.detach();
+        var target = $('#square' + destination);
+        draggedItem.appendTo(target);
+        var isWinner = checkForWinner(source, destination);
+
+        if (isWinner) {
+            updateResults(source);
         }
         else if (_turnsLeft > 0) {
-            changePlayerTurn(sourceLocation);
+            changeTurn(source);
         }
         else {
             endCurrentRound();
         }
     }
 
-    function resetBoard() {
-        $('#gameBoard').children().remove();
-        $('.square').children().remove();
+    function checkForWinner(player, firstTile) {
+        var isWon = false;
+        var tilesToCheck;
+        var winning_tiles = getWinningTiles();
+        tilesToCheck = winning_tiles(firstTile);
+
+        for (var i = 0; i < tilesToCheck.length; i++) {
+            secondTile = firstTile + parseInt(tilesToCheck[i].shift());
+            thirdTile = firstTile + parseInt(tilesToCheck[i].shift());
+            if ($('#square' + secondTile).children().text() != player) {
+                isWon = false;
+            }
+            else if ($('#square' + thirdTile).children().text() != player) {
+                isWon = false;
+            }
+            else {
+                highlightWinningTiles([firstTile, secondTile, thirdTile]);
+                return true;
+            }
+                
+        }
+        return isWon;                
     }
 
-    function createBoard() {
-        for (var i = 0; i < SQUARE_COUNT; i++) {
-            var $square = $('<div id="square' + i + '" data-square="' + i + '" class="square" ></div>');
-            $square.appendTo($('#gameBoard'));
+    function highlightWinningTiles(winningTiles) {
+        for (var i = 0; i < winningTiles.length; i++) {
+            $('#square' + winningTiles[i]).children().addClass("won-tile");
         }
     }
 
-    function displayResults(wonPlayer) {
+    function updateResults(winner) {
+        if (winner == "X")
+            _resultX++;
+        else
+            _resultO++;
+        _totalPlayedRounds++;
+        displayRoundResults(winner);
+    }
+
+    function displayRoundResults(winner) {
         $('#resultX').text(_resultX);
         $('#resultO').text(_resultO);
         $('#totalRounds').text(_totalPlayedRounds);
         $("#btnPlayAgain").css("display", "");
-        switch (wonPlayer) {
+        switch (winner) {
             case "X":
                 $('#message').text($('#firstPlayer').text() + " has won this round!");
-                toggleResultColor(wonPlayer);
+                toggleResultColor(winner);
                 break;
             case "O":
                 $('#message').text($('#secondPlayer').text() + " has won this round!");
-                toggleResultColor(wonPlayer);
+                toggleResultColor(winner);
                 break;
             case "0":
-                $('#message').text("No player has won! Try again");
-                toggleResultColor(wonPlayer);
+                $('#message').text("It's a tie! Try again.");
+                toggleResultColor(winner);
                 break;
         }
     }
@@ -156,217 +269,25 @@ var MyTicTacToe = (function ($) {
         }
     }
 
-    function playAgain() {
-        GAME_HUB.server.initializeRound();
-    }
-
-    function drop(e) {
-        e.preventDefault();
-        GAME_HUB.server.executeTurn(e.target.id, e.dataTransfer.getData('text'));
-    }
-
-    function dragStarted(e) {
-        var $tile = $(e.target);
-        $tile.addClass("dragged");
-        var sourceLocation = $tile.parent().data('square');
-        e.dataTransfer.setData('text', sourceLocation.toString());
-        e.dataTransfer.effectAllowed = 'move';
-    }
-
-
-    function dragEnded(e) {
-        $(e.target).removeClass("dragged");
-    }
-
-    function preventDefault(e) {
-        e.preventDefault();
-    }
-
-    function updateResults(wonPlayer) {
-        if (wonPlayer == "X")
-            _resultX++;
-        else
-            _resultO++;
-        _totalPlayedRounds++;
-        displayResults(wonPlayer);
-    }
-
-    function changePlayerTurn(previousPlayer) {
+    function changeTurn(previousPlayer) {
         if (previousPlayer == "X")
-            _playerLetter = "O";
+            _player = "O";
         else
-            _playerLetter = "X";
-        initializePlayer(_playerLetter);
+            _player = "X";
+        setPlayer(_player);
     }
 
     function endCurrentRound() {
         _totalPlayedRounds++;
-        displayResults("0");
-    }
+        displayRoundResults("0");
+    }    
 
-    function checkForWinner(player, squareTile) {
-        var $square = $('#square' + squareTile);
-        var result;
-        switch (true) {
-            case checkHorizontalTiles(player, squareTile):
-                result = true;
-                break;
-            case checkVerticalTiles(player, squareTile):
-                result = true;
-                break;
-            case checkDiagonalTiles(player, squareTile):
-                result = true;
-                break;
-            default:
-                result = false;
-        }
-        return result;
-    }
-
-    function checkHorizontalTiles(player, squareTile) {
-        var secondTile;
-        var thirdTile;
-
-        // tile is in the first column
-        if (squareTile % 3 == 0) {
-            secondTile = squareTile + 1;
-            thirdTile = secondTile + 1;
-        }
-        else if (squareTile % 3 == 1) { // tile is in the middle column
-            secondTile = squareTile - 1;
-            thirdTile = squareTile + 1;
-        }
-        else if (squareTile % 3 == 2) { // tile is in the third column
-            secondTile = squareTile - 1;
-            thirdTile = secondTile - 1;
-        }
-
-        return checkWinningTiles(player, squareTile, secondTile, thirdTile);
-    }
-
-    function checkVerticalTiles(player, squareTile) {
-        var secondTile;
-        var thirdTile;
-
-        // tile is in the first row
-        if (squareTile < 3) {
-            secondTile = squareTile + 3;
-            thirdTile = secondTile + 3;
-        }
-        else if (squareTile < 6) { // tile is in the middle row
-            secondTile = squareTile - 3;
-            thirdTile = squareTile + 3;
-        }
-        else { // tile is in the third row
-            secondTile = squareTile - 3;
-            thirdTile = secondTile - 3;
-        }
-
-        return checkWinningTiles(player, squareTile, secondTile, thirdTile);
-    }
-
-    function checkDiagonalTiles(player, squareTile) {
-        var secondTile;
-        var thirdTile;
-
-        // tile is in the first row
-        if (squareTile < 3) {
-            if (squareTile == 0) { // left corner tile
-                secondTile = squareTile + 4;
-                thirdTile = secondTile + 4;
-            }
-            else if (squareTile == 2) { // right corner tile
-                secondTile = squareTile + 2;
-                thirdTile = secondTile + 2;
-            }
-            else
-                return;
-        }
-        else if (squareTile == 4) { // middle tile in second row -- exceptional case
-            secondTile = [squareTile - 2, squareTile - 4]; // outmost right or outmost left tiles in first row
-            thirdTile = [squareTile + 2, squareTile + 4];  // outmost right or outmost left tiles in third row
-        }
-        else if (squareTile > 5) { // tile is in the third row
-            if (squareTile == 6) {
-                secondTile = squareTile - 2;
-                thirdTile = secondTile - 2;
-            }
-            else if (squareTile == 8) {
-                secondTile = squareTile - 4;
-                thirdTile = secondTile - 4;
-            }
-            else
-                return;
-        }
-        else
-            return;
-
-        return checkWinningTiles(player, squareTile, secondTile, thirdTile);
-    }
-
-    function checkWinningTiles(player, firstTile, secondTile, thirdTile) {
-        if (secondTile.length > 1 && thirdTile.length > 1) {
-            var secondTileDiagonal;
-            var thirdTileDiagonal;
-            var diagonalWon = false;
-
-            if ($('#square' + secondTile[0]).children().text() == player && $('#square' + thirdTile[0]).children().text() == player) {
-                secondTileDiagonal = secondTile[0];
-                thirdTileDiagonal = thirdTile[0];
-                diagonalWon = true;
-            }
-
-            if ($('#square' + secondTile[1]).children().text() == player && $('#square' + thirdTile[1]).children().text() == player) {
-                secondTileDiagonal = secondTile[1];
-                thirdTileDiagonal = thirdTile[1];
-                diagonalWon = true;
-            }
-
-            if (diagonalWon) {
-                $('#square' + firstTile).children().addClass("won-tile");
-                $('#square' + secondTileDiagonal).children().addClass("won-tile");
-                $('#square' + thirdTileDiagonal).children().addClass("won-tile");
-                return true;
-            }
-            else {
-                return false;
-            }
-
-        }
-        else {
-            if ($('#square' + secondTile).children().text() != player) {
-                return false;
-            }
-            if ($('#square' + thirdTile).children().text() != player) {
-                return false;
-            }
-
-            $('#square' + firstTile).children().addClass("won-tile");
-            $('#square' + secondTile).children().addClass("won-tile");
-            $('#square' + thirdTile).children().addClass("won-tile");
-        }
-        return true;
-    }
-
-    function isPlayerTurn(tilePlayer, playerInTurn) {
-        if (tilePlayer.length && _currentPlayer == playerInTurn) {
-            return true;
-        }
-        else
-            return false;
-    }
-
-    function isMobile() {
-        if (screen.width <= 550)
-            _isMobile = true;
-        else
-            _isMobile = false;
-
-        return _isMobile;
+    function playAgain() {
+        GAME_HUB.server.initializeRound();
     }
 
 })(jQuery);
 
 $(document).ready(function () {
-    MyTicTacToe.startGame();
+    MyTicTacToe.setGame();
 });
